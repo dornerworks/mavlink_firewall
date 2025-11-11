5960453a97a4a3f1370f2a3eb17d76cc774ea731
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2025-11-11 11:35:24 -0500

    Clean up a bit and add comments

--- mavlink_firewall/src/main.rs
+++ mavlink_firewall/src/main.rs
@@ -1,4 +1,3 @@
-use mavlink_parser_vest::spec_mavlink_msg_msg;
 use mavlink_parser_vest::{
     MavCmd, MavlinkMsg, MavlinkMsgMsg, MavlinkV1MsgPayload, MavlinkV2MsgPayload, parse_mavlink_msg,
 };
@@ -85,6 +84,16 @@ fn firewall(packet: &[u8]) -> (r: bool)
             // However, I am not sure how I can relate either bufs to the parsed object
             assert(msg@.msg is MavLink2 ==> mavlink_is_v2(out_buf));
 
+            assert(spec_mavlink_msg().spec_parse(packet@).is_some() && spec_mavlink_msg().spec_parse(packet@).unwrap().1.msg is MavLink2 ==> mavlink_is_v2(packet@));
+
+            // I wanted to see if doing the actual executable serialization changed anything about the relation. It did not.
+            // let mut outbuf = Vec::new();
+            // if let Ok(out_len) = serialize_mavlink_msg(&msg, &mut outbuf, 0) {
+            //     assert(len == out_len);
+            //     assert(packet@.take(len as int) == outbuf@.take(len as int));
+            //     assert(msg@.msg is MavLink2 ==> mavlink_is_v2(outbuf@.take(len as int)));
+            // }
+
             // assert(spec_gumbo_msg_is_mav_cmd_flash_bootloader(packet@) ==> spec_msg_is_flash_bootloader(msg@));
             // assert(spec_msg_is_flash_bootloader(msg@) ==> spec_gumbo_msg_is_mav_cmd_flash_bootloader(s));
 
@@ -141,7 +150,6 @@ pub open spec fn two_bytes_to_u16(
   (((byte1) as u16) * 256u16 + ((byte0) as u16)) as u16
 }
 
-
 pub open spec fn three_bytes_to_u32(
   byte0: u8,
   byte1: u8,
@@ -152,25 +160,18 @@ pub open spec fn three_bytes_to_u32(
 }
 
 pub open spec fn msg_is_command_int(msg: Seq<u8>) -> bool {
-    // msg.subrange(7, 10) =~= seq![75,0,0]
     three_bytes_to_u32(msg[7], msg[8], msg[9]) == 75u32
-
-    // msg.subrange(7, 10) =~= seq![0,0,75]
 }
 
 pub open spec fn command_int_msg_is_bootloader_flash(msg: Seq<u8>) -> bool {
-    // two_bytes_to_u16(msg[13], msg[12]) == 42650u16
     two_bytes_to_u16(msg[13], msg[14]) == 42650u16
 }
 
 pub open spec fn msg_is_command_long(msg: Seq<u8>) -> bool {
-    // msg.subrange(7, 10) =~= seq![76,0,0]
     three_bytes_to_u32(msg[7], msg[8], msg[9]) == 76u32
-    // msg.subrange(7, 10) =~= seq![0,0,76]
 }
 
 pub open spec fn command_long_msg_is_bootloader_flash(msg: Seq<u8>) -> bool {
-    // two_bytes_to_u16(msg[14], msg[13]) == 42650u16
     two_bytes_to_u16(msg[12], msg[13]) == 42650u16
 }
 
@@ -181,8 +182,9 @@ pub open spec fn mavlink_is_v2(msg: Seq<u8>) -> bool
 
 pub open spec fn spec_gumbo_msg_is_mav_cmd_flash_bootloader(msg: Seq<u8>) -> bool
 {
-mavlink_is_v2(msg) && ((msg_is_command_int(msg) && command_int_msg_is_bootloader_flash(msg)) ||
-    (msg_is_command_long(msg) && command_long_msg_is_bootloader_flash(msg)))
+    mavlink_is_v2(msg) &&
+        ((msg_is_command_int(msg) && command_int_msg_is_bootloader_flash(msg)) ||
+        (msg_is_command_long(msg) && command_long_msg_is_bootloader_flash(msg)))
 }
 
 
@@ -190,38 +192,18 @@ pub fn time_triggered(packet: &[u8]) -> (r: bool)
     requires
         packet.len() > 20,
     ensures
+        // Testing outside of the HAMR generated code means we don't have API calls.
+        // However, we assume this is the same as having data on the input port and
+        // the boolean return value is whether it is copied on the output port.
+        //
+        // So the following should be equivalent to:
+        // ((HasEvent(In0) && msg_is_mav_cmd_flash_bootloader(In0.payload)) ->:
+        //  (NoSend(Out0)));
         spec_gumbo_msg_is_mav_cmd_flash_bootloader(packet@) ==> (r == false),
 {
 
-    // let msg_id = ((((packet[9]) as u32) * 65536u32 + ((packet[8]) as u32) * 256u32 + ((packet[7]) as u32))) as u32;
-    // let mav_cmd = (( ((packet[13]) as u16) * 256u16 + ((packet[12]) as u16))) as u16;
-    // print_msg_id(msg_id);
-    // print_mav_cmd(mav_cmd);
     firewall(packet)
 }
-
-
-    // let mut i = 0;
-    // while i < msg.len()
-    //  invariant
-    //     0 <= i <= msg@.len(),
-    //     forall |j| 0 <= j < i ==> packet@[j] == msg@[j],
-    // decreases
-    //     msg@.len() - i
-    // {
-    //     packet[i] = msg[i];
-    //     i += 1;
-    // }
-
-    #[verifier::external_body]
-    fn print_msg_id(val: u32) {
-        println!("msg_id: {val}");
-    }
-
-    #[verifier::external_body]
-    fn print_mav_cmd(val: u16) {
-        println!("mav_cmd: {val}");
-    }
 }
 
 fn main() {
